6123b18ec1e17c9948e98a5c438c52cf38bccbde
Tobias <tobiaslegenden@gmail.com>
2024-06-16 21:29:43 +0200

    excel data checked

--- src/actions/importExcelCsvActions.js
+++ src/actions/importExcelCsvActions.js
@@ -61,7 +61,8 @@ export const dropExcelFile = (file) => (dispatch) => {
                 adults: adults,
                 group: group,
                 checkIn: formattedDate,
-                checkOut: checkOut
+                checkOut: checkOut,
+                checked: false
             });
         }
 
