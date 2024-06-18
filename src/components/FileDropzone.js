b655b3a2167c1981adc31b8782502c4ed9bb1586
Tobias <tobiaslegenden@gmail.com>
2024-06-18 23:06:47 +0200

    new text and onClick

--- src/components/FileDropzone.js
+++ src/components/FileDropzone.js
@@ -1,5 +1,3 @@
-// src/components/FileDropzone.js
-
 import React from 'react';
 import './FileDropzone.css';
 
@@ -25,22 +23,40 @@ class FileDropzone extends React.Component {
         }
     }
 
+    handleClick = () => {
+        this.fileInput.click();
+    }
+
+    setFileInputRef = (element) => {
+        this.fileInput = element;
+    }
+
     render() {
-        const { disabled, property, date } = this.props;
+        const { disabled, property, date, fileType } = this.props;
+        let dropzoneText = '';
+
+        if (fileType === 'csv') {
+            dropzoneText = 'Tryk for at åbne, eller træk og slip SiteMinder fil (csv).';
+        } else if (fileType === 'excel') {
+            dropzoneText = 'Tryk for at åbne, eller træk og slip Spectra AnkomstListe fil (XLS).';
+        }
 
         return (
             <div
                 className={`file-dropzone ${disabled ? 'disabled' : ''}`}
                 onDrop={this.handleDrop}
                 onDragOver={this.handleDragOver}
+                onClick={this.handleClick}
             >
                 <input
                     type="file"
                     accept=".csv, .xlsx, .xls"
                     onChange={this.handleChange}
                     disabled={disabled}
+                    ref={this.setFileInputRef}
+                    style={{ display: 'none' }}
                 />
-                <p>{property || date || 'Drag and drop a file here, or click to select a file.'}</p>
+                <p>{property || date || dropzoneText}</p>
             </div>
         );
     }
